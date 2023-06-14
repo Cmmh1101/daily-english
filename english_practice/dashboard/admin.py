c62bf3b53a3d67418f1ac6145a7f23c99395372c
Carla Montano <cm.emprendiendo@gmail.com>
2023-06-13 23:27:42 -0500

    added sets and flashcards models

--- english_practice/dashboard/admin.py
+++ english_practice/dashboard/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, FlashcardSet, Flashcard
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(FlashcardSet)
+admin.site.register(Flashcard)
--- english_practice/dashboard/models.py
+++ english_practice/dashboard/models.py
@@ -6,3 +6,18 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+class FlashcardSet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=400, null=True)
+
+    def __str__(self):
+        return self.topic
+
+class Flashcard(models.Model):
+    set = models.ForeignKey(FlashcardSet, on_delete=models.CASCADE)
+    word_or_phrase = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.word_or_phrase} word in {self.set}"
